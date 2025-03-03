6ff484fad89ad64e113b976a3672f0c252d3467e
Ben Smith <bnsmth235@gmail.com>
2025-03-03 11:45:47 -0600

    Build fix

--- bnsmth.me/src/App.tsx
+++ bnsmth.me/src/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
       <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet"></link>
       <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       <Navbar />
