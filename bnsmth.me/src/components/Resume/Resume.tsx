b16a82e03d725cd29aae329b237bbea16066abe3
Ben Smith <bnsmth235@gmail.com>
2025-03-04 20:48:47 -0600

    Dependency fix and some tweaks

--- bnsmth.me/src/components/Resume/Resume.tsx
+++ bnsmth.me/src/components/Resume/Resume.tsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useRef } from "react";
 import resume from "../../assets/resume.pdf";
 import * as pdfjsLib from "pdfjs-dist/build/pdf";
-import "pdfjs-dist/build/pdf.worker.entry";
+//import "pdfjs-dist/build/pdf.worker";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 
 const Resume: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const renderThumbnail = async () => {
-      const pdf = await pdfjsLib.getDocument(resume).promise;
-      const page = await pdf.getPage(1);
-      const viewport = page.getViewport({ scale: 0.5 });
-      const canvas = canvasRef.current;
-      const context = canvas?.getContext("2d");
+      try {
+        const pdf = await pdfjsLib.getDocument(resume).promise;
+        const page = await pdf.getPage(1);
+        const viewport = page.getViewport({ scale: 1 });
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext("2d");
 
-      if (canvas && context) {
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        const renderContext = {
-          canvasContext: context,
-          viewport: viewport,
-        };
-        await page.render(renderContext).promise;
+        if (canvas && context) {
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+          context.setTransform(1, 0, 0, -1, 0, viewport.height); // Flip the image vertically
+          const renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+          };
+          await page.render(renderContext).promise;
+        }
+      } catch (error) {
+        console.error("Error rendering PDF thumbnail:", error);
       }
     };
 
@@ -35,7 +42,7 @@ const Resume: React.FC = () => {
         {/* Add your about me description here */}
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
       </p>
-      <canvas ref={canvasRef} className="mb-4 w-full max-w-full"></canvas>
+      <canvas ref={canvasRef} className="mb-4 w-1/4 max-w-full"></canvas>
       <a href={resume} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
         Download Resume
       </a>
